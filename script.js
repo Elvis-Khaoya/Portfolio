b74c43c32c727c2512f49ebe7f2e479c4019b7ec
Elvis-Khaoya <elviskhaoya@gmail.com>
2025-10-05 17:47:08 +0300

    Update portfolio files (index.html, style.css, script.js,elvis.jpg)

--- script.js
+++ script.js
@@ -71,3 +71,14 @@ window.onload = function() {
         });
     }
 };
+// BLOG TOGGLE FUNCTIONALITY
+function toggleBlogContent(button) {
+  const content = button.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+    button.textContent = "Read More";
+  } else {
+    content.style.display = "block";
+    button.textContent = "Read Less";
+  }
+}
